199a9c27f634e87a5336cd03ef80b02bf24c35c7
Kathleen Repko <kathleen.repko@gmail.com>
2018-02-13 14:36:20 -0500

    Added JS to clear form after submitting.

---
+++ HighPoint1/secondary.js
@@ -0,0 +1,8 @@
+﻿
+    //function submitClear() {
+    //        var frm = document.getElementsByName('contactForm')[0];
+    //        frm.submit(); // Submit the form
+    //        frm.reset();  // Reset all form data
+    //        return false; // Prevent page refresh
+
+    //    }
